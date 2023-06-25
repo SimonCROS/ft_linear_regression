f47d65f1f894e92809d722726e7f2b0201a62b08
Simon CROS <simon.dc.cros@gmail.com>
2023-06-25 23:30:24 +0200

    train (get theta values)

--- train.py
+++ train.py
@@ -1,17 +1,19 @@
 import sys
+import math
 
 import csv
 from PySide6.QtWidgets import QApplication
-
-from ui import GraphWindow
 
 theta0: float = 0
 theta1: float = 0
 
 x_name: str
 y_name: str
-x: list[float] = []
-y: list[float] = []
+input: list[float] = []
+targets: list[float] = []
+
+learning_rate = 0.1
+iterations_count = 1000
 
 def is_float(string):
     try:
@@ -26,34 +28,61 @@ def estimate_price(mileage: float) -> float:
     return theta0 + (theta1 * mileage)
 
 if __name__ == "__main__":
-    with open('data.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
+    try:
+        with open('data.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
 
-        for row in csv_reader:
-            if len(row) != 2:
-                print(f"Wrong line {line_count} ({row})", file=sys.stderr)
-                exit(1)
-            if line_count == 0:
-                x_name = row[0]
-                y_name = row[1]
-                line_count += 1
-            else:
-                if not is_float(row[0]):
-                    print(f"Wrong line {line_count} ({row[0]}) is not a valid float", file=sys.stderr)
+            for row in csv_reader:
+                if len(row) != 2:
+                    print(f"Wrong line {line_count} ({row})", file=sys.stderr)
                     exit(1)
-                if not is_float(row[1]):
-                    print(f"Wrong line {line_count} ({row[1]}) is not a valid float", file=sys.stderr)
-                    exit(1)
-                x.append(float(row[0]))
-                y.append(float(row[1]))
-                line_count += 1
+                if line_count == 0:
+                    x_name = row[0]
+                    y_name = row[1]
+                    line_count += 1
+                else:
+                    if not is_float(row[0]):
+                        print(f"Wrong line {line_count} ({row[0]}) is not a valid float", file=sys.stderr)
+                        exit(1)
+                    if not is_float(row[1]):
+                        print(f"Wrong line {line_count} ({row[1]}) is not a valid float", file=sys.stderr)
+                        exit(1)
+                    input.append(float(row[0]))
+                    targets.append(float(row[1]))
+                    line_count += 1
+    except Exception as ex:
+        print(ex.strerror, file=sys.stderr)
+        exit(1)
 
     print(f'Processed {line_count} lines.')
 
-    app = QApplication(sys.argv)
-    w = GraphWindow(x_name, x, y_name, y)
-    w.show()
-    app.exec()
+    samples_count = len(input)
+    
+    mean = sum(input) / len(input)
+    variance = sum(pow(x - mean, 2) for x in input) / len(input)
+    std = math.sqrt(variance)
+    
+    normalized_input = [(x - mean) / std for x in input]
 
-    tmp0 = 1 * (1 / len(y))
+    for i in range(iterations_count):
+        tmp0 = 0
+        tmp1 = 0
+
+        for (value, observed) in zip(normalized_input, targets):
+            # print(value, estimate_price(value), observed)
+            estimated = estimate_price(value)
+            diff = estimated - observed
+
+            tmp0 += diff
+            tmp1 += diff * value
+
+        theta0 -= learning_rate * (1 / samples_count) * tmp0
+        theta1 -= learning_rate * (1 / samples_count) * tmp1
+
+    print(f"theta0 = {theta0}, theta1 = {theta1}")
+
+    # app = QApplication(sys.argv)
+    # w = GraphWindow(x_name, x, y_name, y)
+    # w.show()
+    # app.exec()
--- ui.py
+++ ui.py
@@ -9,9 +9,6 @@ class GraphWindow(QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
 
-        hour = [1,2,3,4,5,6,7,8,9,10]
-        temperature = [30,32,34,32,33,31,29,32,35,45]
-
         self.graphWidget.setBackground('w')
 
         self.graphWidget.plot(x, y, pen=None, symbol='o')
