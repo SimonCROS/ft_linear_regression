d5676e3a9a43c5de7023290d3b104bca6f7f6aa4
Simon CROS <simon.dc.cros@gmail.com>
2023-06-28 00:48:31 +0200

    Use min-max instead of z-score normalization

--- train.py
+++ train.py
@@ -9,16 +9,13 @@ from ui import GraphWindow
 theta0: float = 0
 theta1: float = 0
 
-mean: float = 0
-std: float = 0
-
 inputs_name: str
 targets_name: str
 input: list[float] = []
 targets: list[float] = []
 
 learning_rate = 0.1
-iterations_count = 100
+iterations_count = 1000
 
 def is_float(string):
     try:
@@ -31,11 +28,6 @@ def estimate_price(value: float) -> float:
     global theta0
     global theta1
     return theta0 + (theta1 * value)
-
-def normalize(value):
-    global mean
-    global std
-    return (value - mean) / std
 
 if __name__ == "__main__":
     try:
@@ -69,18 +61,15 @@ if __name__ == "__main__":
 
     samples_count = len(input)
     
-    mean = sum(input) / len(input)
-    variance = sum(pow(x - mean, 2) for x in input) / len(input)
-    std = math.sqrt(variance)
-    
-    # Normalise will set the mean to 0 and the standard deviation at 1
-    normalized_input = list(map(normalize, input))
+    # Normalise using min-max method
+    normalized_input = [(x - min(input)) / (max(input) - min(input)) for x in input]
+    normalized_targets = [(y - min(targets)) / (max(targets) - min(targets)) for y in targets]
 
     for i in range(iterations_count):
         tmp0 = 0
         tmp1 = 0
 
-        for (value, observed) in zip(normalized_input, targets):
+        for (value, observed) in zip(normalized_input, normalized_targets):
             # print(value, estimate_price(value), observed)
             estimated = estimate_price(value)
             diff = estimated - observed
@@ -91,10 +80,17 @@ if __name__ == "__main__":
         theta0 -= learning_rate * (1 / samples_count) * tmp0
         theta1 -= learning_rate * (1 / samples_count) * tmp1
 
+    input_diff = max(input) - min(input)
+    targets_diff = max(targets) - min(targets)
+
+    theta1 = theta1 * targets_diff / input_diff
+    theta0 = theta0 * targets_diff + min(targets) - theta1 * min(input)
+
     print(f"theta0 = {theta0}, theta1 = {theta1}")
+    print(estimate_price(0))
 
     app = QApplication(sys.argv)
     w = GraphWindow(inputs_name, input, targets_name, targets)
-    w.drawLine([min(input), estimate_price(normalize(min(input)))], [max(input), estimate_price(normalize(max(input)))])
+    w.drawLine([min(input), estimate_price(min(input))], [max(input), estimate_price(max(input))])
     w.show()
     app.exec()
